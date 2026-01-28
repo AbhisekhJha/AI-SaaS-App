4720f7b65a8bd5ccb7729e5254544009c170b9e1
MeAbhiJha <iamabhisekh11@gmail.com>
2026-01-28 15:46:06 +0545

    fix(openai): correct formatting of OpenAI configuration adding timeout

--- server/utils/openai.ts
+++ server/utils/openai.ts
@@ -4,8 +4,7 @@ const config = useRuntimeConfig();
 
 export const openai = new OpenAI({
     apiKey: config.geminiApiKey,
-    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
-
-
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+    timeout: 60000
 });
 
