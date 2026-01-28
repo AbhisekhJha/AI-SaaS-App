03d0f128d933a53fb71bc01e538fa89aaa35541a
MeAbhiJha <iamabhisekh11@gmail.com>
2026-01-28 15:42:05 +0545

    feat(config): add color mode configuration for dark preference

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
+  colorMode: {
+    preference: 'dark',
+    fallback: 'dark',
+    storageKey: 'nuxt-color-mode',
+  },
     vite: {
     plugins: [
       tailwindcss(),
